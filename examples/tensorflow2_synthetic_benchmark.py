0272eed7e08712b1a075928141da04d9afce8501
DEKHTIARJonathan <contact@jonathandekhtiar.eu>
2020-04-06 16:01:37 -0700

    Initial Commit

---
+++ examples/tensorflow2_synthetic_benchmark.py
@@ -0,0 +1,142 @@
+# Copyright 2019 Uber Technologies, Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+from __future__ import absolute_import, division, print_function
+
+import argparse
+import os
+import numpy as np
+import timeit
+
+import tensorflow as tf
+import horovod.tensorflow as hvd
+from tensorflow.keras import applications
+
+from tensorflow.keras.mixed_precision import experimental as mixed_precision
+
+# Benchmark settings
+parser = argparse.ArgumentParser(description='TensorFlow Synthetic Benchmark',
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--fp16-allreduce', action='store_true', default=False,
+                    help='use fp16 compression during allreduce')
+
+parser.add_argument('--model', type=str, default='ResNet50',
+                    help='model to benchmark')
+
+parser.add_argument('--batch-size', type=int, default=32,
+                    help='input batch size')
+
+parser.add_argument('--num-iters', type=int, default=5,
+                    help='number of benchmark iterations')
+
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                    help='disables CUDA training')
+
+parser.add_argument('--use-amp', action='store_true', default=False,
+                    help='enable AMP computation')
+
+args = parser.parse_args()
+args.cuda = not args.no_cuda
+
+if args.use_amp:
+    policy = mixed_precision.Policy('mixed_float16')
+    mixed_precision.set_policy(policy)
+
+# Horovod: initialize Horovod.
+hvd.init()
+
+# Horovod: pin GPU to be used to process local rank (one GPU per process)
+if args.cuda:
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    if gpus:
+        tf.config.experimental.set_visible_devices(gpus[hvd.local_rank()], 'GPU')
+else:
+    os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+# Set up standard model.
+model = getattr(applications, args.model)(weights=None)
+opt = tf.optimizers.SGD(0.01)
+
+if args.use_amp:
+    opt = mixed_precision.LossScaleOptimizer(opt, loss_scale='dynamic')
+
+data = tf.random.uniform([args.batch_size, 224, 224, 3])
+target = tf.random.uniform([args.batch_size, 1], minval=0, maxval=999, dtype=tf.int64)
+
+
+@tf.function
+def benchmark_step(first_batch):
+    # Horovod: (optional) compression algorithm.
+    compression = hvd.Compression.fp16 if args.fp16_allreduce else hvd.Compression.none
+
+    # Horovod: use DistributedGradientTape
+    with tf.GradientTape() as tape:
+        probs = model(data, training=True)
+        loss = tf.losses.categorical_crossentropy(target, probs)
+
+        if args.use_amp:
+            loss = opt.get_scaled_loss(loss)
+
+    # Horovod: add Horovod Distributed GradientTape.
+    tape = hvd.DistributedGradientTape(tape, compression=compression)
+
+    gradients = tape.gradient(loss, model.trainable_variables)
+
+    if args.use_amp:
+        gradients = opt.get_unscaled_gradients(gradients)
+
+    opt.apply_gradients(zip(gradients, model.trainable_variables))
+
+    # Horovod: broadcast initial variable states from rank 0 to all other processes.
+    # This is necessary to ensure consistent initialization of all workers when
+    # training is started with random weights or restored from a checkpoint.
+    #
+    # Note: broadcast should be done after the first gradient step to ensure optimizer
+    # initialization.
+    if first_batch:
+        hvd.broadcast_variables(model.variables, root_rank=0)
+        hvd.broadcast_variables(opt.variables(), root_rank=0)
+
+
+def log(s, nl=True):
+    if hvd.rank() != 0:
+        return
+    print(s, end='\n' if nl else '')
+
+
+log('Model: %s' % args.model)
+log('Batch size: %d' % args.batch_size)
+device = 'GPU' if args.cuda else 'CPU'
+log('Number of %ss: %d' % (device, hvd.size()))
+
+
+with tf.device(device):
+    # Benchmark
+    log('Running benchmark...')
+    img_secs = []
+    for x in range(args.num_iters):
+        time = timeit.timeit(
+            lambda: benchmark_step(first_batch=x == 0),
+            number=1
+        )
+        img_sec = args.batch_size / time
+        log('Iter #%d: %.1f img/sec per %s' % (x, img_sec, device))
+        img_secs.append(img_sec)
+
+    # Results
+    img_sec_mean = np.mean(img_secs)
+    img_sec_conf = 1.96 * np.std(img_secs)
+    log('Img/sec per %s: %.1f +-%.1f' % (device, img_sec_mean, img_sec_conf))
+    log('Total img/sec on %d %s(s): %.1f +-%.1f' %
+        (hvd.size(), device, hvd.size() * img_sec_mean, hvd.size() * img_sec_conf))
---
+++ test.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import subprocess
+import sys
+import unittest
+
+from parameterized import parameterized
+from parameterized import param
+
+
+__all__ = [
+    'CustomTestCase',
+]
+
+SUCCESS_CODE = 0
+SIGKILL_CODE = 9
+
+
+class CustomTestCase(unittest.TestCase):
+
+    def run_command(self, job_name, cmdline_flags):
+
+        def exec_cmd(cmd):
+            command_proc = subprocess.Popen(cmd)
+            return_code = command_proc.wait()
+
+            if return_code not in [SUCCESS_CODE, SIGKILL_CODE]:
+                sys.tracebacklimit = 0
+                stdout, stderr = command_proc.communicate()
+                raise RuntimeError(
+                    "\n##################################################\n"
+                    "[*] STDOUT:{error_stdout}\n"
+                    "[*] STERR:{error_stderr}\n"
+                    "[*] command launched: `{command}`\n"
+                    "##################################################\n".format(
+                        error_stdout=stdout.decode("utf-8"),
+                        error_stderr=stderr.decode("utf-8"),
+                        command=" ".join(cmd)
+                    )
+                )
+
+            return True
+
+        py_command = "python examples/%s.py %s" % (job_name, cmdline_flags)
+        py_command = py_command.strip()
+        run_command = py_command.split(" ")
+
+        print("Command Executed: %s" % (" ".join(run_command)), file=sys.stderr)
+
+        self.assertTrue(exec_cmd(run_command))
+
+        return True
+
+    @parameterized.expand([
+        # tensorflow2_synthetic_benchmark
+        param(
+            "RN50 - Gradient Tape + HVD",
+            filename="tensorflow2_synthetic_benchmark",
+            cmdline_flags=""
+        ),
+        param(
+            "RN50 - Gradient Tape + HVD + AMP",
+            filename="tensorflow2_synthetic_benchmark",
+            cmdline_flags="--use-amp"
+        ),
+        param(
+            "RN50 - Gradient Tape + HVD + AMP + FP16 All Reduce",
+            filename="tensorflow2_synthetic_benchmark",
+            cmdline_flags="--use-amp --fp16-allreduce"
+        ),
+        # tensorflow2_mnist
+        param(
+            "keras.Sequential CTL - Gradient Tape + HVD",
+            filename="tensorflow2_mnist",
+            cmdline_flags=""
+        ),
+        param(
+            "keras.Sequential CTL - Gradient Tape + HVD + AMP",
+            filename="tensorflow2_mnist",
+            cmdline_flags="--use-amp"
+        ),
+        param(
+            "keras.Sequential CTL - Gradient Tape + HVD + AMP + FP16 All Reduce",
+            filename="tensorflow2_mnist",
+            cmdline_flags="--use-amp --fp16-allreduce"
+        ),
+        # tensorflow2_keras_mnist
+        param(
+            "keras fit & compile - Gradient Tape + HVD",
+            filename="tensorflow2_keras_mnist",
+            cmdline_flags=""
+        ),
+        param(
+            "keras fit & compile - Gradient Tape + HVD + AMP",
+            filename="tensorflow2_keras_mnist",
+            cmdline_flags="--use-amp"
+        ),
+        param(
+            "keras fit & compile - Gradient Tape + HVD + AMP + FP16 All Reduce",
+            filename="tensorflow2_keras_mnist",
+            cmdline_flags="--use-amp --fp16-allreduce"
+        )
+    ])
+    def test_example(self, _, filename, cmdline_flags):
+        self.run_command(job_name=filename, cmdline_flags=cmdline_flags)
